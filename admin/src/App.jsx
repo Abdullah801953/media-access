fb4ef9ba270e8addf8ac8ec8831af2ef86a3aaaa
Abdullah801953 <ak786shariq@gmail.com>
2025-08-14 09:10:16 +0530

    comm-3

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/admin" element={<Dashboard/>}/>
+        <Route path="/" element={<Dashboard/>}/>
       </Routes>
     </Router>
   );
